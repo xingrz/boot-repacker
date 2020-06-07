795bb03cf1f49354880ac72011da3fc127b010d1
XiNGRZ <chenxingyu92@gmail.com>
2020-06-07 13:32:25 +0800

    Set up project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import vuetify from './plugins/vuetify';
 
-Vue.config.productionTip = false
+import App from './App.vue';
+
+Vue.config.productionTip = false;
 
 new Vue({
+  vuetify,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
